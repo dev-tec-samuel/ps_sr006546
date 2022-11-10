38bed58526c1278a3406738ac53dea6834ba3a63
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-09 21:08:45 -0300

    Concluído método de salvamento de registros

--- public/index.php
+++ public/index.php
@@ -4,12 +4,8 @@ use PetShop\Model\Dica;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-
 $dica = new Dica();
-$dica->loadById(1);
-echo $dica->iddica . PHP_EOL;
-echo $dica->titulo . PHP_EOL;
-echo $dica->descricao . PHP_EOL;
-echo $dica->created_at . PHP_EOL;
-echo $dica->updated_at . PHP_EOL;
+$dica->loadById(2);
+//$dica->titulo = 'Dica Atualizada';
+$dica->descricao = 'NOVA DESCRIÇÃO';
+$dica->save();
