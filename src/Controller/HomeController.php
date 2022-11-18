03819373de57ab04a3257702f5f94b9eba03ef46
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-18 20:10:02 -0300

    Criada classe abstrata p/ renderizar topo e rodapé

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,22 @@
 <?php
 namespace Petshop\Controller;
 
+use Petshop\Core\FrontController;
 use Petshop\Model\Estado;
 use Petshop\View\Render;
 
-class HomeController
+class HomeController extends FrontController
 {
   public function index()
   {
-    $objEstado = new Estado();
-    $estados = $objEstado->find();
+    $estados = (new Estado())->find();
+
 
     $dados = [];
     $dados['titulo'] = 'Lista de Estados';
     $dados['estados'] = $estados;
+    $dados['topo'] = $this->carregaHTMLTopo();
+    $dados['rodape'] = $this->carregaHTMLRodape();
     
     Render::front('home', $dados);
   }
---
+++ src/Core/FrontController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Petshop\Core;
+
+use Petshop\Model\Empresa;
+use Petshop\View\Render;
+
+abstract class FrontController 
+{
+  /**
+   * Alimenta com dados e renderiza o topo do site
+   * para o cliente (front-end)
+   *
+   * @return void
+   */
+  public function carregaHTMLTopo()
+  {
+    $empresa = new Empresa();
+    $dados = $empresa->find(['tipo='=>'Matriz']);
+    return Render::block('topo', $dados[0]);
+  }
+
+  /**
+   * Alimenta com dados e renderiza o topo do site
+   * para o cliente (front-end)
+   *
+   * @return void
+   */
+  public function carregaHTMLRodape()
+  {
+    $empresa = new Empresa();
+    $dados = $empresa->find(['tipo='=>'Matriz']);
+    return Render::block('rodape', $dados[0]);
+  }
+
+}
