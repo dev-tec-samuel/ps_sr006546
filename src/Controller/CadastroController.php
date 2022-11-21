f14dcbbdc19d128611e0f1d256ed1747c6f4d3cb
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-21 20:25:04 -0300

    Criada estrutura para cadastro de cliente

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -20,6 +20,57 @@ class CadastroController extends FrontController
 
   private function formCadastro()
   {
-    
+    $dados = [
+      'btn_label' => 'Criar minha Conta',
+      'btn_class' => 'btn btn-success mt-5',
+      'fields' => [
+        [
+          'type' => 'radio-inline',
+          'class' => 'col-4',
+          'label' => 'Você é pessoa...',
+          'name' => 'tipo',
+          'options' => [
+            ['label' => 'Física', 'value' => 'F'],
+            ['label' => 'Jurídica', 'value' => 'J']
+          ],
+          'required' => true
+        ],
+        [
+          'type' => 'text',
+          'class' => 'col-6',
+          'label' => 'Documento',
+          'name' => 'cpfcnpj',
+          'required' => true
+        ],
+        [
+          'type' => 'text',
+          'name' => 'nome',
+          'label' => 'Seu nome completo',
+          'required' => true
+        ],
+        [
+          'type' => 'email',
+          'name' => 'email',
+          'label' => 'Seu E-mail mais usado',
+          'required' => true
+        ],
+        [
+          'type' => 'password',
+          'class' => 'col-6',
+          'name' => 'senha',
+          'label' => 'Crie uma senha',
+          'required' => true
+        ],
+        [
+          'type' => 'password',
+          'class' => 'col-6',
+          'name' => 'senha2',
+          'label' => 'Confirme sua senha',
+          'required' => true
+        ],
+      ]
+    ];
+
+    return Render::block('form', $dados);
   }
 }
---
+++ templates/frontend/pages/cadastro.php
@@ -0,0 +1,12 @@
+<div class="container my-5">
+
+  <div class="row">
+    <div class="col-6 mx-auto">
+      <h1 class="text-center display-6 pb-2 border-bottom mb-4">
+        Criar conta
+      </h1>
+      <?= $formCadastro ?>
+    </div>
+  </div>
+
+</div>
