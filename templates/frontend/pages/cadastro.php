dfa2f384b44b0125355c4db18d438bcbac637df9
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-21 21:09:10 -0300

    Add área para impressão de msg

--- templates/frontend/pages/cadastro.php
+++ templates/frontend/pages/cadastro.php
@@ -2,9 +2,8 @@
 
   <div class="row">
     <div class="col-6 mx-auto">
-      <h1 class="text-center display-6 pb-2 border-bottom mb-4">
-        Criar conta
-      </h1>
+      <h1 class="text-center display-6 pb-2 border-bottom mb-4">Criar conta</h1>
+      <?= retornaHTMLAlertMenssagemSessao() ?>
       <?= $formCadastro ?>
     </div>
   </div>
