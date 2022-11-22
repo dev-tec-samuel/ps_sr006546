00c17c4974fc5abf9305ceb1ee0eebed7fc7da0f
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-22 19:14:31 -0300

    Criada função de redirecionamento com msg

--- resources/funcoes.php
+++ resources/funcoes.php
@@ -25,3 +25,25 @@ function retornaHTMLAlertMenssagemSessao()
   
   return $bootstrapAlert;
 }
+
+/**
+ * Função que redireciona (via header location) para uma
+ * url específica
+ *
+ * @param string $detino   URL destino
+ * @param string $tipoMsg (primary, secondary, success, danger, warning, info, light, dark)
+ * @param string $mensagem
+ * @return void
+ */
+function redireciona(string $detino, string $tipoMsg='', string $mensagem='')
+{
+  if($tipoMsg && $mensagem) {
+    $_SESSION['mensagem'] = [
+      'tipo'  => $tipoMsg,
+      'texto' => $mensagem
+    ];
+  }
+  
+  header('location:' . $detino);
+  exit;
+}
