20045a6c7bf35aec02e25d43f4c3c3b1442eff64
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-24 19:54:37 -0300

    Add recurso para uso de recursos TEXTAREA

--- templates/frontend/blocks/form.php
+++ templates/frontend/blocks/form.php
@@ -86,7 +86,16 @@
                         </div>
                         HTML;
                 break;
-
+            case 'text-area':
+                $rows = $f['rows'] ?? '';
+                $cols = $f['cols'] ?? '';
+                echo <<<HTML
+                        <div class="{$f['class']}">
+                            <label for="{$f['name']}" class="form-label">{$f['label']}{$f['txt_required']}</label>
+                            <textarea class="form-control" id="{$f['name']}" name="{$f['name']}" placeholder="{$f['placeholder']}" rows="{$rows}" cols="{$cols}" {$f['required']}>{$postValue}</textarea>
+                        </div>
+                    HTML;
+                break;
         }
     }
 ?>
