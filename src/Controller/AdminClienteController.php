6b4117ad575d153f18c06ce7c6486b08d4800196
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-25 20:40:35 -0300

    Usando o bloco de listagem de dados do /admin

--- src/Controller/AdminClienteController.php
+++ src/Controller/AdminClienteController.php
@@ -1,14 +1,29 @@
 <?php
 namespace Petshop\Controller;
 
+use Petshop\Model\Cliente;
 use Petshop\View\Render;
 
 class AdminClienteController
 {
   public function listar()
   {
+    //alimentando dados para a tabela de listagem
+    $dadosListagem = [];
+    $dadosListagem['objeto']  = new Cliente();
+    $dadosListagem['colunas'] = [
+      ['campo'=>'idcliente',  'class'=>'text-center'],
+      ['campo'=>'tipo',       'class'=>'text-center'],
+      ['campo'=>'nome'],
+      ['campo'=>'email'],
+      ['campo'=>'created_at', 'class'=>'text-center'],
+    ];
+    $htmlTabela = Render::block('tabela-admin', $dadosListagem);
+
+    //alimentando dados para a página de clientes
     $dados = [];
     $dados['titulo'] = 'Clientes';
+    $dados['tabela'] = $htmlTabela;
 
     Render::back('clientes', $dados);
   }
