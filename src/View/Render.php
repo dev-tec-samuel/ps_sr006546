123c5b58c81ee64ed027bfd4d6d8f549e5cc9d5c
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-25 19:32:44 -0300

    Alterado caminho de blocos

--- src/View/Render.php
+++ src/View/Render.php
@@ -47,7 +47,7 @@ class Render
   static public function block(string $bloco, array $dados=[]) 
   {
     // monta o caminho do local onde o bloco solicitado está localizado
-    $pathArquivo = TFRONTEND . 'blocks/' . $bloco . '.php';
+    $pathArquivo = TEMPLATES . 'blocks/' . $bloco . '.php';
 
     if (!file_exists($pathArquivo)) {
       error_log('O bloco não foi localizado em: ' . $pathArquivo);
