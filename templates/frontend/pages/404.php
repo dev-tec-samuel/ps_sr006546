6f603d3e3071ae6da03b611917b6b8299e30dc60
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-28 19:21:12 -0300

    Add style na pag. erro404

--- templates/frontend/pages/404.php
+++ templates/frontend/pages/404.php
@@ -1,7 +1,7 @@
 <?php $link = URL . $_SERVER['REQUEST_URI'] ?>
 
 <div class="container text-center" style="margin: 8em auto">
-  <h1 class="display-4"> Página não encontrada</h1>
+  <h1 class="display-4"> <div class="icon icon-generic"></div> Página não encontrada</h1>
   <p class="h5 mt-5"> Erro 404: A página "<?= $link ?>" solicitado não existe</p>
   <p class="lead mt-5">Faça uma busca no site ou pesquise pelo menu</p>
 </div>
