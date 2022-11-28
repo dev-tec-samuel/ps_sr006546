5698560452907528661400f94478f972888bcc89
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-28 20:09:37 -0300

    Criada estrutura para manutenção de usuários

--- src/Core/App.php
+++ src/Core/App.php
@@ -70,10 +70,14 @@ class App
   {
     self::$router->mount('/admin', function() {
       self::$router->get('/dashboard', '\Petshop\Controller\AdminDashboardController@index');
+
       self::$router->get('/clientes', '\Petshop\Controller\AdminClienteController@listar');
       self::$router->get('/clientes/{valor}', '\Petshop\Controller\AdminClienteController@form');
-
       self::$router->post('/clientes/{valor}', '\Petshop\Controller\AdminClienteController@postForm');
+
+      self::$router->get('/usuarios', '\Petshop\Controller\AdminUsuarioController@listar');
+      self::$router->get('/usuarios/{valor}', '\Petshop\Controller\AdminUsuarioController@form');
+      self::$router->post('/usuarios/{valor}', '\Petshop\Controller\AdminUsuarioController@postForm');
 
     });
   }
--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -4,6 +4,8 @@ namespace Petshop\Model;
 use Petshop\Core\Attribute\Campo;
 use Petshop\Core\Attribute\Entidade;
 use Petshop\Core\DAO;
+use Petshop\Core\Exception;
+use Respect\Validation\Validator as v;
 
 #[Entidade(name: 'usuarios')]
 class Usuario extends DAO
@@ -11,19 +13,19 @@ class Usuario extends DAO
   #[Campo(label: 'Cód. Usuário', pk: true, nn: true, auto: true)]
   protected $idUsuario;
 
-  #[Campo(label: 'Nome do usuário', nn: true)]
+  #[Campo(label: 'Nome Completo', nn: true)]
   protected $nome;
 
-  #[Campo(label: 'E-mail do usuário', nn: true)]
+  #[Campo(label: 'E-mail', nn: true)]
   protected $email;
 
-  #[Campo(label: 'Senha do usuário', nn: true)]
+  #[Campo(label: 'Senha', nn: true)]
   protected $senha;
 
-  #[Campo(label: 'Tipo do usuário', nn: true)]
+  #[Campo(label: 'Tipo', nn: true)]
   protected $tipo;
 
-  #[Campo(label: 'Qtd. acessos do usuário', nn: true)]
+  #[Campo(label: 'Qtd. acessos', nn: true)]
   protected $qtdAcessos;
 
   #[Campo(label: 'Dt. Criação', nn: true, auto: true)]
@@ -43,7 +45,7 @@ class Usuario extends DAO
     return $this->nome;
   }
 
-  public function setNome($nome): self
+  public function setNome(string $nome): self
   {
     $this->nome = $nome;
 
@@ -55,10 +57,16 @@ class Usuario extends DAO
     return $this->email;
   }
 
-  public function setEmail($email): self
+  public function setEmail(string $email): self
   {
-    $this->email = $email;
+    $email = strtolower($email);
 
+    $emailValido = v::email()->validate($email);
+    if (!$emailValido) {
+      throw new Exception('O e-mail informado é inválido');
+    }
+
+    $this->email = $email;
     return $this;
   }
 
@@ -67,10 +75,18 @@ class Usuario extends DAO
     return $this->senha;
   }
 
-  public function setSenha($senha): self
+  public function setSenha(string $senha): self
   {
-    $this->senha = $senha;
+    if($this->senha && !$senha) {
+      return $this;
+    }
+    if (strlen($senha) < 5) {
+      throw new Exception('O comprimento da senha é inválido, digite ao menos cinco caracteres');
+    }
 
+    $hashdaSenha = hash_hmac('md5', $senha, SALT_SENHA);
+    $senha = password_hash($hashdaSenha, PASSWORD_DEFAULT);
+    $this->senha = $senha;
     return $this;
   }
 
@@ -79,10 +95,14 @@ class Usuario extends DAO
     return $this->tipo;
   }
 
-  public function setTipo($tipo): self
+  public function setTipo(string $tipo): self
   {
-    $this->tipo = $tipo;
+    $tipo = trim($tipo);
+    if (!in_array($tipo, ['Gestor', 'Vendedor'])) {
+      throw new Exception('O tipo de pessoa não está definido corretamente');
+    }
 
+    $this->tipo = $tipo;
     return $this;
   }
 
@@ -91,7 +111,7 @@ class Usuario extends DAO
     return $this->qtdAcessos;
   }
 
-  public function setQtdAcessos($qtdAcessos): self
+  public function setQtdAcessos(int $qtdAcessos): self
   {
     $this->qtdAcessos = $qtdAcessos;
 
