8a3f3e3784a16f440ea35c0ca89ccfc2083c8c99
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-11-29 21:58:00 -0300

    Ajuste de css

--- templates/frontend/pages/404.php
+++ templates/frontend/pages/404.php
@@ -3,5 +3,6 @@
 <div class="container text-center" style="margin: 8em auto">
   <h1 class="display-4"> <div class="icon icon-generic"></div> Página não encontrada</h1>
   <p class="h5 mt-5"> Erro 404: A página "<?= $link ?>" solicitado não existe</p>
-  <p class="lead mt-5">Faça uma busca no site ou pesquise pelo menu</p>
+  <p class="lead mt-5 mb-3">Faça uma busca no site ou pesquise pelo menu</p>
+  <a href="/" class="btn btn-primary btn-lg rounded-pill ">Voltar para casa</a>
 </div>
--- templates/frontend/pages/meus-dados.php
+++ templates/frontend/pages/meus-dados.php
@@ -1,16 +1,26 @@
-<div class="container my-5">
-  <div class="row">
-    <div class="col-3 text-center">
-      <div>Bem vindo(a): <strong><?= $cliente['prinome']?></strong></div>
-      <div style="font-size:.8em;">(<?= $cliente['email']?>)</div>
-      <div class="mt-5">
-        <a href="/logout" class="badge text-bg-warning text-decoration-none">SAIR</a>
+<div class="container my-5 ps-5">
+  <div class="row d-flex">
+    <div class="icon-perfil d-flex align-items-center mb-3">
+      <i class="fa-solid fa-id-card col-2 text-end ps-4" style="transform: scale(3.5);"></i>
+      <h1 class="col-7 text-align-left ps-0">Meus dados</h1>
+    </div>
+    <div class="coluna-nome d-flex align-items-center mb-3">
+      <div class="col-2 text-end pe-3"><strong>NOME</strong></div>
+      <div class="col-7">
+        <span class=" form-control"><?= $cliente['nome']?></span>
       </div>
     </div>
-    <div class="col-9 ps-5">
-      - teste<br>
-      - teste<br>
-      - teste
+    <div class="coluna-email d-flex align-items-center mb-3">
+      <div class="col-2 text-end pe-3"><strong>E-MAIL</strong></div>
+      <div class="col-7">
+        <span class=" form-control"><?= $cliente['email']?></span>
+      </div>
+    </div>
+    <div class="coluna-documento d-flex align-items-center">
+      <div class="col-2 text-end pe-3"><strong>DOCUMENTO</strong></div>
+      <div class="col-7">
+        <span class=" form-control"><?= $cliente['cpfcnpj']?></span>
+      </div>
     </div>
   </div>
 </div>
