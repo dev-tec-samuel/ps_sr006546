2300f2b6b03f1369c30d791d50b28992378fbf33
Samuel Filipe Drumm da Rosa <sr006546@cfjl.com>
2022-12-01 20:43:26 -0300

    Criado formulário e vies de formulário

--- src/Controller/AdminImagemController.php
+++ src/Controller/AdminImagemController.php
@@ -36,7 +36,7 @@ class AdminImagemController
     $dados = [];
     $dados['titulo'] = 'Imagens - Listagem';
     $campoOrdenacao = $objetoComFiguras->getOrderByField();
-    $dados['registroAlvo'] = $model . ': <u>' . $objetoComFiguras->$campoOrdenacao . '</u>';
+    $dados['registroAlvo'] = $model . ': <u class="d-inline-block mb-1">>' . $objetoComFiguras->$campoOrdenacao . '</u>';
     $dados['usuario'] = $_SESSION['usuario'];
     $dados['tabela'] = $htmlTabela;
 
@@ -45,22 +45,32 @@ class AdminImagemController
 
   public function form($model, $idmodel, $valor)
   {
+    $modelPath = "Petshop\\Model\\{$model}";
+    if(!class_exists($modelPath)) {
+      redireciona('admin/dashboard', 'danger', 'Página não localizada. Classe de dados destino não definida');
+    }
+
+    $objetoComFiguras = new $modelPath;
+    $objetoComFiguras->loadById($idmodel);
+
     //verificar se o parâmetro tem um número e, se for número, é um ID válido
     if (is_numeric($valor)) {
-      $objeto = new Categoria();
-      $resultado = $objeto->find(['idcategoria=' => $valor]);
+      $objeto = new Arquivo();
+      $resultado = $objeto->find(['idarquivo=' => $valor]);
       if (empty($resultado)) {
-        redireciona('/admin/categorias', 'danger', 'Link inválido, registro não localizado');
+        redireciona("/admin/imagens{$model}/{$idmodel}", 'danger', 'Link inválido, registro não localizado');
       }
       $_POST = $resultado[0];
     }
 
     //cria e exibe o formulário
     $dados = [];
-    $dados['titulo'] = 'Categorias - Manutenção';
+    $dados['titulo'] = 'Imagens - Manutenção';
     $dados['formulario'] = $this->renderizaFormulario(empty($_POST));
+    $campoOrdenacao = $objetoComFiguras->getOrderByField();
+    $dados['registroAlvo'] = $model . ': <u class="d-inline-block mb-1">' . $objetoComFiguras->$campoOrdenacao . '</u>';
 
-    Render::back('categorias', $dados);
+    Render::back('imagens', $dados);
   }
 
   public function postForm($model, $idmodel, $valor)
@@ -100,12 +110,15 @@ class AdminImagemController
   public function renderizaFormulario($novo)
   {
     $dados = [
-      'btn_class' => 'btn btn-warning px-5 mt-5',
+      'btn_class' => 'btn btn-warning px-5 my-5',
       'btn_label' => ($novo ? 'Adicionar' : 'Atualizar'),
+      'enctype'   => 'multipart/form-data',
       'fields' => [
-        ['type' => 'readonly', 'name' => 'idcategoria', 'class' => 'col-3', 'label' => 'Id. Categoria'],
-        ['type' => 'text', 'name' => 'nome', 'class' => 'col-5', 'label' => 'Nome completo', 'required'=>true],
-        ['type' => 'text', 'name' => 'descricao', 'class' => 'col-6', 'label' => 'Descrição'],
+        ['type' => 'readonly', 'name' => 'idarquivo', 'class' => 'col-3', 'label' => 'Id. Arquivo'],
+        ['type' => 'readonly', 'name' => 'nome', 'class' => 'col-4', 'label' => 'Nome do arquivo (automático)'],
+        ['type' => 'file', 'name' => 'arquivo', 'class' => 'col-5', 'label' => 'Arquivo', 'accept'=>'image/*'],
+        ['type' => 'readonly', 'name' => 'tipo', 'class' => 'col-2'],
+        ['type' => 'textarea', 'name' => 'descricao', 'class' => 'col-12', 'label' => 'Descrição', 'rows'=>'5'],
         ['type' => 'readonly', 'name' => 'created_at', 'class' => 'col-3', 'label' => 'Criado em:'],
         ['type' => 'readonly', 'name' => 'updated_at', 'class' => 'col-3', 'label' => 'Atualizado em:'],
       ]
